39d11b4bd58121b52b096894567deb4215975945
unitrixx <loewe.sch@gmx.de>
2018-11-29 11:02:59 +0100

    Sudoku

--- Uebungen/Sudoku/src/SudokuSolver.java
+++ Uebungen/Sudoku/src/SudokuSolver.java
@@ -7,11 +7,20 @@ public class SudokuSolver {
     public boolean isSolvable(Sudoku sudoku, int i, int j){
         if (i >= 9 && j >= 9)
             return false;
+        if (i > 8)
+            return isSolvable(sudoku, 0, j+1);
         if (sudoku.isWritable(i, j)){
-
+            int n = 1;
+            while (conflictAt(sudoku, i, j, n){
+                if (n >= 9)
+                    return false;
+                n++;
+            }
+            sudoku.setValue(i, j, n);
+            return isSolvable(sudoku, i+1, j) || isSolvable(sudoku, i-1, j);
         }
         if (!sudoku.isWritable(i,j)){
-            return isSolvable(sudoku,i+1, j) || isSolvable(sudoku, i, j+1);
+            return isSolvable(sudoku,i+1, j);
         }
         if (i == 8 && j == 8){
             boolean result = false;
@@ -24,23 +33,23 @@ public class SudokuSolver {
 
     public boolean conflictAt(Sudoku sudoku, int i, int j, int value){
         if (value > 9 || value < 1)
-            return false;
+            return true;
         if (!sudoku.isWritable(i, j))
-            return false;
+            return true;
         for (int b = 0; b < 9; b++){
             if (sudoku.getValue(i, b) == value)
-                return false;
+                return true;
         }
         for (int a = 0; a < 9; a++){
             if (sudoku.getValue(a, j) == value)
-                return false;
+                return true;
         }
         for (int r = i- i%3; r <= i - i%3 + 2; r++){
             for (int c = j - j%3; c <= j - j%3 + 2; c++){
                 if (sudoku.getValue(r, c) == value)
-                    return false;
+                    return true;
             }
         }
-        return true;
+        return false;
     }
 }
